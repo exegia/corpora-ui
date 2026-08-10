aeea4f972a4c7072f69f1ec45bb1d40225c0f699
Emmanuel De Freitas <manny.defreitas7@me.com>
2026-08-10 12:40:33 -0700

    feat(component): add BrowserFrame and supporting utilities
    Introduce the `BrowserFrame` component for simulating a browser-like UI. Includes a title bar with configurable styles (`hidden`, `titlebar`, `expanded`) and support for displaying a URL or title. Added accompanying type definitions (`IBrowserProps`) and utility functions (`titleBarStyle`) for customization.
    
    Signed-off-by: Emmanuel De Freitas <manny.defreitas7@me.com>

---
+++ react/src/components/beste/piece/utils.ts
@@ -0,0 +1,23 @@
+import type { ClassNameValue } from "tailwind-merge"
+import type { TitleStyleType } from "./type"
+
+export const titleBarStyle = (
+  style: TitleStyleType,
+  component: "title" | "frame"
+): ClassNameValue => {
+  const titleBarClassName: Record<TitleStyleType, ClassNameValue> = {
+    titlebar: "",
+    hidden: "top-0 left-0 absolute",
+    expanded: "",
+  }
+
+  const bodyClassName: Record<TitleStyleType, ClassNameValue> = {
+    titlebar: "",
+    hidden: "",
+    expanded: "",
+  }
+
+  const cssName =
+    component == "title" ? titleBarClassName[style] : bodyClassName[style]
+  return cssName
+}
